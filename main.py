36cf7fc5db067901dc2acda8f7e709d7b9716cac
Alibaba Abdulbasit <reachalibee@gmail.com>
2026-01-05 13:58:52 +0100

    Data Ingestion Added

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to my custom log")
+
+STAGE_NAME = "Data Ingestion Stage"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>> stage {STAGE_NAME} <<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
