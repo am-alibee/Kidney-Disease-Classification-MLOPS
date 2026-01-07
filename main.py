4a02e77a1486957dacaf60da807b90019a703ce3
Alibaba Abdulbasit <reachalibee@gmail.com>
2026-01-07 22:53:12 +0100

    model training added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -20,8 +21,21 @@ STAGE_NAME = "Prepare base model"
 if __name__ == "__main__":
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = PrepareBaseModelTrainingPipeline()
-        obj.main()
+        prepare_base_model = PrepareBaseModelTrainingPipeline()
+        prepare_base_model.main()
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e    
+
+
+STAGE_NAME = "Training"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        model_trainer = ModelTrainingPipeline()
+        model_trainer.main()
         logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<")
     except Exception as e:
         logger.exception(e)
--- src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
+++ src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
@@ -20,10 +20,10 @@ class PrepareBaseModelTrainingPipeline:
 
 if __name__ == "__main__":
     try:
-        logger.info(f">>>>>> stage {STAGE_NAME} <<<<<<")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = ConfigurationManager()
         obj.main()
-        logger.info(f">>>>>>> stage {STAGE_NAME} <<<<<<")
+        logger.info(f">>>>>>> stage {STAGE_NAME} ended <<<<<<")
     except Exception as e:
         logger.exception(e)
         raise e
